b13cffb0f498c086806661bfbbd8e0a20fa81527
Yash <yashkadam@ee.iitb.ac.in>
2023-03-28 15:35:08 +0000

    Nothing significant

--- resex/resex/settings.py
+++ resex/resex/settings.py
@@ -137,5 +137,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-auth.iitb.ac.in'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = '213079001@iitb.ac.in'
-EMAIL_HOST_PASSWORD = 'Y@$hSM-X800'
+EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASS")
